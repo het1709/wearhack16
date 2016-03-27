7a92e63a2632a4c32dc829ed28a711a798abb6ce
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2016-03-26 20:45:17 -0400

    Finalize

--- opencv/car_camera.py
+++ opencv/car_camera.py
@@ -13,6 +13,7 @@ import cv2
 import datetime
 import time
 import copy
+
 # from flask import Flask
 #
 # app = Flask(__name__)
@@ -24,10 +25,11 @@ import copy
 # if __name__ == "__main__":
 #     app.run()
 
+SCALE_VAL = 2.00
 WEB_CAM_INDEX = 0
-WIN_STRIDE_VAL = 3
-NEAREST_POINT = WIN_STRIDE_VAL * 13
-FRAME_WIDTH = 500
+WIN_STRIDE_VAL = 4
+NEAREST_POINT = 315
+FRAME_WIDTH = 600
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -50,57 +52,43 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 firstFrame = None
 
 # loop over the frames of the video
-# while True:
 # grab the current frame and initialize the occupied/unoccupied
 # text
 (grabbed, frame) = camera.read()
 
-# if the frame could not be grabbed, then we have reached the end
-# of the video
-# if not grabbed:
-# 	break
 
 # resize the frame, convert it to grayscale, and blur it
 frame = imutils.resize(frame, width=min(FRAME_WIDTH, frame.shape[1]))
-# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
 # detect people in the image
-# (rects, weights) = hog.detectMultiScale(frame, winStride=(WIN_STRIDE_VAL, WIN_STRIDE_VAL),
-# 	padding=(8, 8), scale=1.05)
-print ('Read')
+(rects, weights) = hog.detectMultiScale(frame, winStride=(WIN_STRIDE_VAL, WIN_STRIDE_VAL),
+    padding=(8, 8), scale=SCALE_VAL)
 
-# orig = frame.copy()
-#
-# # draw the original bounding boxes
-# for (x, y, w, h) in rects:
-# 	print ('Detected')
-# 	if y >= 40:
-# 		cv2.rectangle(orig, (x, y), (x + w, y + h), (255, 0, 0), 2)
-# 	else:
-# 		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#
-# # apply non-maxima suppression to the bounding boxes using a
-# # fairly large overlap threshold to try to maintain overlapping
-# # boxes that are still people
-# rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-# pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-#
-# # draw the final bounding boxes
-# for (xA, yA, xB, yB) in pick:
-# 	print (yA)
-# 	if yA < NEAREST_POINT:
-# 		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-# 	else:
-# 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+orig = frame.copy()
+
+# draw the original bounding boxes
+for (x, y, w, h) in rects:
+	if ( y + h ) < NEAREST_POINT:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	else:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# apply non-maxima suppression to the bounding boxes using a
+# fairly large overlap threshold to try to maintain overlapping
+# boxes that are still people
+rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+# draw the final bounding boxes
+for (xA, yA, xB, yB) in pick:
+	if ( y + h ) < NEAREST_POINT:
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	else:
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 while True:
-	cv2.imshow("Pedestrian Detector", frame)
-key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Pedestrian Detector", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-# # if the `q` key is pressed, break from the lop
-# if key == ord("q"):
-# 	break
-# break
 
 # cleanup the camera and close any open windows
 camera.release()
